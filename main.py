bfd22f98a2e7dd00a7d765caad28664f50b15b62
ZZ3n <ndsrhkd@naver.com>
2024-05-30 14:09:45 +0900

    hotfix sync problem?

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True  # 음성 상태 인텐트 활성화
 
+
 class MyBot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix='/', intents=intents)
@@ -28,20 +29,26 @@ class MyBot(commands.Bot):
 
     async def setup_hook(self):
         # 슬래시 명령어 동기화
-        # guild = discord.Object(id=GUILD_ID)
+        for guild in self.guilds:
+            self.tree.copy_global_to(guild=guild)
         await self.tree.sync()
+
+        # guild = discord.Object(id=GUILD_ID)
         # self.tree.copy_global_to(guild=guild)
         # await self.tree.sync(guild=guild)
+
 
 bot = MyBot()
 
 # 명령어 설정
 setup_commands(bot)
 
+
 @bot.event
 async def on_ready():
     print(f'Logged on as {bot.user}!')
     await bot.change_presence(status=discord.Status.online, activity=discord.Game("대기중"))
+
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -49,5 +56,6 @@ async def on_command_error(ctx, error):
         await ctx.send("해당 명령어를 찾을 수 없습니다.", delete_after=10)
     else:
         await ctx.send(f"명령어 실행 중 오류가 발생했습니다: {error}", delete_after=10)
+
 
 bot.run(TOKEN)
